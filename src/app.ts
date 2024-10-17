bb8479e3de529d31534b868d45bf9894251e02f2
rahathossenantor <md.rahathossenantor@gmail.com>
2024-10-17 19:34:44 +0600

    Initial DB creation completed

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ app.use(express.json());
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to The Ph HealthCare");
+    res.send({
+        success: true,
+        message: "Welcome to The PH HealthCare!",
+    });
 });
 
 export default app;
