b44322de5a03c5b58ee7d4bd44923b979ab15e8d
rahathossenantor <md.rahathossenantor@gmail.com>
2024-10-20 17:48:00 +0600

    Password hasshed and error handled

--- src/app/modules/users/user.controllers.ts
+++ src/app/modules/users/user.controllers.ts
@@ -2,8 +2,20 @@ import { Request, Response } from "express";
 import userServices from "./user.services";
 
 const createAdmin = async (req: Request, res: Response) => {
-    const admin = await userServices.createAdminIntoDB(req.body);
-    return res.send(admin);
+    try {
+        const admin = await userServices.createAdminIntoDB(req.body);
+        return res.status(200).json({
+            success: true,
+            message: "Admin created successfully.",
+            data: admin
+        });
+    } catch (err: any) {
+        return res.status(200).json({
+            success: false,
+            message: err.message || "Something went wrong!",
+            error: err
+        });
+    };
 };
 
 const userControllers = {
--- src/app/modules/users/user.services.ts
+++ src/app/modules/users/user.services.ts
@@ -1,11 +1,18 @@
 import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 12);
+};
 
 const prisma = new PrismaClient();
 
 const createAdminIntoDB = async (payload: any) => {
+    const hashedPassword = await hashPassword(payload.password);
+
     const userData = {
         email: payload.admin.email,
-        password: payload.password,
+        password: hashedPassword,
         role: UserRole.ADMIN
     };
 
