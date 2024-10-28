56df953252cf74a36da69da3870066dff0f6a327
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-10-28 19:10:24 +0600

    Doctor route created

--- src/app/modules/users/user.routes.ts
+++ src/app/modules/users/user.routes.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response, Router } from "express";
 import userControllers from "./user.controllers";
 import auth from "../../middlewares/auth";
 import { upload } from "../../utils/uploadImage";
+import requestValidator from "../../middlewares/requestValidator";
+import { userValidations } from "./user.validations";
 
 const router = Router();
 
@@ -13,6 +15,18 @@ router.post(
         req.body = JSON.parse(req.body.data);
         next();
     },
+    userControllers.createAdmin
+);
+
+router.post(
+    "/create-doctor",
+    auth("SUPER_ADMIN", "ADMIN"),
+    upload.single("file"),
+    (req: Request, _res: Response, next: NextFunction) => {
+        req.body = JSON.parse(req.body.data);
+        next();
+    },
+    requestValidator(userValidations.doctorCreateSchema),
     userControllers.createAdmin
 );
 
