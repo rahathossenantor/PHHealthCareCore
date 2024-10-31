09df56915089428392f623036f9299b83fd6a912
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-10-31 18:26:15 +0600

    Get all patients controller created

---
+++ src/app/modules/patient/patient.controllers.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import pick from "../../utils/pick";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import { filterAndPaginateOptions } from "../../constants/global.constants";
+import { patientFiltarableFields } from "./patient.constants";
+import patientServices from "./patient.services";
+
+const getAllPatients = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+    const query = pick(req.query, patientFiltarableFields);
+    const options = pick(req.query, filterAndPaginateOptions);
+
+    const response = await patientServices.getAllPatientsFromDB(query, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Patients fetched successfully.",
+        meta: response.meta,
+        data: response.data
+    });
+});
+
+const patientControllers = {
+    getAllPatients
+};
+
+export default patientControllers;
