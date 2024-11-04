255491afaa0a3636203b30dc67a0417a4b7df421
rahathossenantor <md.rahathossenantor@gmail.com>
2024-11-04 22:10:34 +0600

    Create schedule controller created

---
+++ src/app/modules/schedule/schedule.controllers.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import scheduleServices from "./schedule.services";
+
+const createSchedule = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+    const response = await scheduleServices.createScheduleIntoDB(req.body);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Schedule created successfully.",
+        data: response
+    });
+});
+
+const scheduleControllers = {
+  createSchedule
+};
+
+export default scheduleControllers;
