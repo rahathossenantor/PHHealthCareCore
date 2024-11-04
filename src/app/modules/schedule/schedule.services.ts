1efc84c58d8a38dcceb38a5104e33b97afccbe64
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-11-04 21:10:05 +0600

    Create schedule service created

---
+++ src/app/modules/schedule/schedule.services.ts
@@ -0,0 +1,63 @@
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../utils/prisma";
+
+const createScheduleIntoDB = async (payload: any) => {
+    const startDate = new Date(payload.startDate);
+    const endDate = new Date(payload.endDate);
+    const schedules = [];
+
+    while (startDate <= endDate) {
+        const startDateTime = new Date(
+            addMinutes(
+                addHours(
+                    `${format(startDate, "yyyy-MM-dd")}`,
+                    Number(payload.startTime.split(":")[0])
+                ),
+                Number(payload.startTime.split(":")[1])
+            )
+        );
+
+        const endDateTime = new Date(
+            addMinutes(
+                addHours(
+                    `${format(startDate, "yyyy-MM-dd")}`,
+                    Number(payload.endTime.split(":")[0])
+                ),
+                Number(payload.endTime.split(":")[1])
+            )
+        );
+
+        while (startDateTime < endDateTime) {
+            const schedule = {
+                startDateTime,
+                endDateTime: addMinutes(startDateTime, 30)
+            };
+
+            const existingSchedule = await prisma.schedule.findFirst({
+                where: {
+                    startDateTime: schedule.startDateTime,
+                    endDateTime: schedule.endDateTime
+                }
+            });
+
+            if (!existingSchedule) {
+                const dbRes = await prisma.schedule.create({
+                    data: schedule
+                });
+                schedules.push(dbRes);
+            };
+
+            startDateTime.setMinutes(startDateTime.getMinutes() + 30);
+        };
+
+        startDate.setDate(startDate.getDate() + 1);
+    };
+
+    return schedules;
+};
+
+const scheduleServices = {
+    createScheduleIntoDB
+};
+
+export default scheduleServices;
