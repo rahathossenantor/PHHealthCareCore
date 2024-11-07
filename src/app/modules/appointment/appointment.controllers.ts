7eda55923b5540cce10bbeba5c152f013f5dc177
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-11-08 00:22:24 +0600

    Get my appointments controller created

--- src/app/modules/appointment/appointment.controllers.ts
+++ src/app/modules/appointment/appointment.controllers.ts
@@ -4,6 +4,8 @@ import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import { TTokenPayload } from "../../types/global.types";
 import appointmentServices from "./appointment.services";
+import pick from "../../utils/pick";
+import { filterAndPaginateOptions } from "../../constants/global.constants";
 
 const createAppointment = catchAsync(async (req: Request & { user?: TTokenPayload }, res: Response, _next: NextFunction) => {
     const response = await appointmentServices.createAppointmentIntoDB(req.user as TTokenPayload, req.body);
@@ -16,8 +18,23 @@ const createAppointment = catchAsync(async (req: Request & { user?: TTokenPayloa
     });
 });
 
+const getMyAppointments = catchAsync(async (req: Request & { user?: TTokenPayload }, res: Response, _next: NextFunction) => {
+    const query = pick(req.query, ["status", "paymentStatus"]);
+    const options = pick(req.query, filterAndPaginateOptions);
+
+    const response = await appointmentServices.getMyAppointmentsFromDB(req.user as TTokenPayload, query, options);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Appointments fetched successfully.",
+        data: response
+    });
+});
+
 const appointmentControllers = {
-    createAppointment
+    createAppointment,
+    getMyAppointments
 };
 
 export default appointmentControllers;
